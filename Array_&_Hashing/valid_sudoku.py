a964c7851c3c4224baf24557997b1fc03a2c7327
john2708 <jbmiranda22796@gmail.com>
2024-06-06 22:39:16 -0700

    sudoku

--- Array_&_Hashing/valid_sudoku.py
+++ Array_&_Hashing/valid_sudoku.py
@@ -1,43 +1,24 @@
 
-import collections
+# https://leetcode.com/problems/valid-sudoku/
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        row = collections.defaultdict(set)
+        col = collections.defaultdict(set)
+        square = collections.defaultdict(set)
 
-def isValidSudoku(board):
-    cols = collections.defaultdict(set)
-    rows = collections.defaultdict(set)
-    squares = collections.defaultdict(set)
-
-    for r in range(9):
-        for c in range(9):
-            if board[r][c] == ".":
-                continue # ignore
-            if (
-                board[r][c] in rows[r]
-                or board[r][c] in cols[c]
-                or board[r][c] in squares[(r // 3, c // 3)]
-               ):
-                return False
-
-            cols[c].add(board[r][c])
-            rows[r].add(board[r][c])
-            squares[(r // 3, c // 3)].add(board[r][c])
+        for r in range(9):
+            for c in range(9):
+                if (
+                    board[r][c] == "."
+                ):
+                    continue
                 
-    return True
-
-    print(board)
-
-
-
-
-
-input_board = [
- ["5","3",".",".","7",".",".",".","."]
-,["6",".",".","1","9","5",".",".","."]
-,[".","9","8",".",".",".",".","6","."]
-,["8",".",".",".","6",".",".",".","3"]
-,["4",".",".","8",".","3",".",".","1"]
-,["7",".",".",".","2",".",".",".","6"]
-,[".","6",".",".",".",".","2","8","."]
-,[".",".",".","4","1","9",".",".","5"]
-,[".",".",".",".","8",".",".","7","9"]]
-
-print(isValidSudoku(input_board))
+                if (board[r][c] in row[r]
+                 or board[r][c] in col[c]
+                 or board[r][c] in square[(r // 3, c // 3)]
+                ):
+                    return False
+                row[r].add(board[r][c])
+                col[c].add(board[r][c])
+                square[(r // 3, c // 3)].add(board[r][c])
+        return True
