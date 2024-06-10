1f58507964441dcaa8039da6e00926e4d53137ce
john2796 <jbmiranda22796@gmail.com>
2024-06-10 03:57:59 -0700

    Rising Temperature

--- SQL_50/Basic_Joins/basic_joins.sql
+++ SQL_50/Basic_Joins/basic_joins.sql
@@ -8,5 +8,41 @@ from Employees e
 left join EmployeeUNI eu
 on e.id = eu.id;
 
+-- https://leetcode.com/problems/product-sales-analysis-i/description/?envType=study-plan-v2&envId=top-sql-50
+-- # Write your MySQL query statement below
+-- # show report for product_name, year and price for each sales in the sales table
+-- # summary: show product for each sale
+select product_name, year, price from Sales s
+left join Product p
+on s.product_id = p.product_id;
 
+-- https://leetcode.com/problems/customer-who-visited-but-did-not-make-any-transactions/description/?envType=study-plan-v2&envId=top-sql-50
+-- - find the IDs of the users who visited without making any transactions 
+-- - and the number of times they made these types of visits.
+-- - visit_id | customer_ id 
+SELECT customer_id,COUNT(customer_id) AS count_no_trans
+FROM Visits
+LEFT JOIN Transactions ON Visits.visit_id=Transactions.visit_id
+WHERE transaction_id is null
+GROUP BY customer_id;
+
+
+
+-- https://leetcode.com/problems/rising-temperature/description/?envType=study-plan-v2&envId=top-sql-50
+/* 
+ - find all dates' ID with higher temp 
+ - compared to prev dates (yesterday)
+ - return result in any order
+
+ get today and yesterday dates and compare them
+ use sql functions
+ https://www.w3schools.com/sql/func_sqlserver_datediff.asp
+*/
+SELECT w1.id
+FROM Weather w1, Weather w2
+WHERE  DATEDIFF(w1.recordDate, w2.recordDate) = 1 -- get diff in 1 day
+AND w1.temperature > w2.temperature; -- select w1 that are greater in temp from prev day
+
+
+-- https://leetcode.com/problems/average-time-of-process-per-machine/description/?envType=study-plan-v2&envId=top-sql-50
 
