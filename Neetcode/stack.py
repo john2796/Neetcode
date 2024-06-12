0499ffe34694be71960a7dfc36f226eb2c889c2f
john2796 <jbmiranda22796@gmail.com>
2024-06-11 22:55:49 -0700

    min-stack

--- Neetcode/stack.py
+++ Neetcode/stack.py
@@ -15,3 +15,34 @@ class Solution:
                 stack.append(c)
 
         return True if not stack else False
+
+
+# https://leetcode.com/problems/min-stack/
+class MinStack:
+
+    def __init__(self):
+        self.s = []
+        self.min_stack = []
+
+    def push(self, val: int) -> None:
+        self.s.append(val)
+        min_val = min(val, self.min_stack[-1] if self.min_stack else val)
+        self.min_stack.append(min_val)
+
+    def pop(self) -> None:
+        self.s.pop()
+        self.min_stack.pop()
+
+    def top(self) -> int:
+        return self.s[-1]
+
+    def getMin(self) -> int:
+        return self.min_stack[-1]
+
+
+# Your MinStack object will be instantiated and called as such:
+# obj = MinStack()
+# obj.push(val)
+# obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.getMin()
