f4df05a1b89ae46ad706a6b176b434fa2909427f
john2796 <jbmiranda22796@gmail.com>
2024-06-11 20:46:48 -0700

    sliding-window-maximum

--- Neetcode/sliding-window.py
+++ Neetcode/sliding-window.py
@@ -119,3 +119,27 @@ class Solution:
                 l += 1
         l, r = res
         return s[l : r + 1] if resLen != float("infinity") else ""
+
+
+# https://leetcode.com/problems/sliding-window-maximum/
+class Solution:
+    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        q = collections.deque()
+        l = r = 0
+        output = []
+
+        while r < len(nums):
+            # pop smaller values from q
+            while q and nums[q[-1]] < nums[r]:
+                q.pop()
+            q.append(r)
+
+            # remove left val from window
+            if l > q[0]:
+                q.popleft()
+
+            if (r + 1) >= k:
+                output.append(nums[q[0]])
+                l += 1
+            r += 1
+        return output
