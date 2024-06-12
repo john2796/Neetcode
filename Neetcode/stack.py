a16ff63d20fe1232465ba078067bb804de934ce1
john2796 <jbmiranda22796@gmail.com>
2024-06-11 22:21:08 -0700

    valid-parentheses

---
+++ Neetcode/stack.py
@@ -0,0 +1,17 @@
+# https://leetcode.com/problems/valid-parentheses/
+class Solution:
+    def isValid(self, s: str) -> bool:
+        map = {")": "(", "]": "[", "}": "{"}
+
+        stack = []
+
+        for c in s:
+            if c in map:
+                if stack and stack[-1] == map[c]:
+                    stack.pop()
+                else:
+                    return False
+            else:
+                stack.append(c)
+
+        return True if not stack else False
