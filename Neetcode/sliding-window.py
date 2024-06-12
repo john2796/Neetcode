89849e3d085609cbf245e0b0c0e381d55ba3ea47
john2796 <jbmiranda22796@gmail.com>
2024-06-11 19:58:29 -0700

    minimum-window-substring

--- Neetcode/sliding-window.py
+++ Neetcode/sliding-window.py
@@ -78,3 +78,40 @@ class Solution:
             if match == len(cntr):
                 return True
         return False
+
+
+# https://leetcode.com/problems/minimum-window-substring/
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        if t == "":
+            return ""
+        # track t count
+        countT, window = {}, {}
+        for c in t:
+            countT[c] = 1 + countT.get(c, 0)
+
+        have, need = 0, len(countT)
+        res, resLen = [-1, -1], float("infinity")
+        l = 0
+
+        for r in range(len(s)):
+            # expand window add right char into window, increment have if char in countT and window and countT val are same
+            c = s[r]
+            window[c] = 1 + window.get(c, 0)
+
+            if c in countT and window[c] == countT[c]:
+                have += 1
+
+            # shrink window when have and need are equal , update result when window length < resLen, pop from the left window
+            while have == need:
+                # update our result
+                if (r - l + 1) < resLen:
+                    res = [l, r]
+                    resLen = r - l + 1
+                # pop from the left of our window
+                window[s[l]] -= 1
+                if s[l] in countT and window[s[l]] < countT[s[l]]:
+                    have -= 1
+                l += 1
+        l, r = res
+        return s[l : r + 1] if resLen != float("infinity") else ""
