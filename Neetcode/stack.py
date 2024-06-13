eb6036d2ba7042cad3160db2f32053c66366b098
john2796 <jbmiranda22796@gmail.com>
2024-06-12 20:43:54 -0700

    generate-parentheses

--- Neetcode/stack.py
+++ Neetcode/stack.py
@@ -70,3 +70,30 @@ class Solution:
                 s.append(int(t))
 
         return s[0]
+
+
+# https://leetcode.com/problems/generate-parentheses/
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        # only add open parenthesis if open < n
+        # only add a closing parenthesis if closed < open
+        # valid IIF open == closed == n
+        stack = []
+        res = []
+
+        def backtrack(openN, closedN):
+            if openN == closedN == n:
+                res.append("".join(stack))
+                return
+            if openN < n:
+                stack.append("(")
+                backtrack(openN + 1, closedN)
+                stack.pop()
+
+            if closedN < openN:
+                stack.append(")")
+                backtrack(openN, closedN + 1)
+                stack.pop()
+
+        backtrack(0, 0)
+        return res
