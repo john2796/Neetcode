5bf06cc061ee0a722256a598b8af773a08c41dda
john2796 <jbmiranda22796@gmail.com>
2024-06-12 22:24:58 -0700

    largest-rectangle-in-histogram

--- Neetcode/stack.py
+++ Neetcode/stack.py
@@ -125,3 +125,24 @@ class Solution:
             if len(stack) >= 2 and stack[-1] <= stack[-2]:
                 stack.pop()
         return len(stack)
+
+
+# https://leetcode.com/problems/largest-rectangle-in-histogram/
+class Solution:
+    def largestRectangleArea(self, heights: List[int]) -> int:
+        max_area = 0
+        stack = []
+        for i, h in enumerate(heights):
+            idx = i
+            if not stack:
+                stack.append([i, h])
+                continue
+            while stack and stack[-1][1] > h:
+                idx, he = stack.pop()
+                max_area = max(max_area, (i - idx) * he)
+            stack.append([idx, h])
+
+        while stack:
+            idx, he = stack.pop()
+            max_area = max(max_area, (i - idx + 1) * he)
+        return max_area
