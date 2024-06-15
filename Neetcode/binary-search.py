5013111a4ce5e5c8338aeb806823c17344647a17
john2796 <jbmiranda22796@gmail.com>
2024-06-15 00:47:58 -0700

    search-a-2d-matrix

---
+++ Neetcode/binary-search.py
@@ -0,0 +1,35 @@
+# https://leetcode.com/problems/binary-search/
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        l, r = 0, len(nums) - 1
+
+        while l <= r:
+            m = (r + l) // 2
+            if nums[m] == target:
+                return m
+            elif nums[m] < target:
+                l = m + 1
+            else:
+                r = m - 1
+        return -1
+
+
+# https://leetcode.com/problems/search-a-2d-matrix/
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        if not matrix:
+            return False
+        m, n = len(matrix), len(matrix[0])
+        l, r = 0, m * n - 1
+
+        while l <= r:
+            mid = (l + r) // 2
+            mid_row, mid_col = divmod(mid, n)
+
+            if matrix[mid_row][mid_col] == target:
+                return True
+            elif matrix[mid_row][mid_col] < target:
+                l = m + 1
+            else:
+                r = m - 1
+        return False
