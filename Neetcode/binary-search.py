57ba671a626eb651c452f6b039bca88256dad0b6
john2708 <jbmiranda22796@gmail.com>
2024-06-15 21:42:13 -0700

    search in rotated sorted array

--- Neetcode/binary-search.py
+++ Neetcode/binary-search.py
@@ -64,3 +64,27 @@ class Solution:
             else:
                 l = m + 1
         return nums[l]
+
+
+# https://leetcode.com/problems/search-in-rotated-sorted-array/
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        l, r = 0, len(nums) - 1
+
+        while l <= r:
+            m = (l+r) // 2
+            if nums[m] == target:
+                return m
+            # check if left half is sorted
+            if nums[l] <= nums[m]:
+                if nums[l] <= target < nums[m]:
+                    r = m - 1
+                else:
+                    l = m + 1
+            # otherwise right half is sorted
+            else:
+                if nums[m] < target <= nums[r]:
+                    l = m + 1
+                else:
+                    r = m - 1
+        return -1
