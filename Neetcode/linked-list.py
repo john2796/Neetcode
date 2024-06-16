f02efa9589019cdab2cfbfa41c3d4a691bd91a97
john2708 <jbmiranda22796@gmail.com>
2024-06-16 00:53:05 -0700

    reorder list

--- Neetcode/linked-list.py
+++ Neetcode/linked-list.py
@@ -38,3 +38,36 @@ class Solution:
         return dummy.next
     
 
+# https://leetcode.com/problems/reorder-list/
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reorderList(self, head: Optional[ListNode]) -> None:
+        """
+        Do not return anything, modify head in-place instead.
+        """
+        # find the middle of the list
+        slow, fast=head,head
+        while fast.next and fast.next.next:
+            slow = slow.next
+            fast = fast.next.next
+
+        # reverse the second half of the list
+        prev, cur = None, slow.next
+        while cur:
+            tmp = cur.next
+            cur.next = prev
+            prev = cur
+            cur = tmp
+        slow.next = None
+
+        # merge 2 halves
+        first ,second = head, prev
+        while second:
+            tmp1, tmp2 = first.next, second.next
+            first.next =second
+            second.next = tmp1
+            first, second = tmp1, tmp2
