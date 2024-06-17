b319b133d3e42b8f0b684fb78e2ec1923f4fe646
john2708 <jbmiranda22796@gmail.com>
2024-06-17 00:06:49 -0700

    linked-list-cycle

--- Neetcode/linked-list.py
+++ Neetcode/linked-list.py
@@ -120,3 +120,21 @@ class Solution:
             l2 = l2.next if l2 else None
 
         return dummy.next
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+# https://leetcode.com/problems/linked-list-cycle/
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        fast, slow = head, head 
+
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+            if slow == fast:
+                return True
+        return False
