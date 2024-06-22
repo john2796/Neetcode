7b21b466794d89d2e579c3bdbe346bb7c45a90b6
john2708 <jbmiranda22796@gmail.com>
2024-06-22 16:15:07 -0700

    copy random list

--- Neetcode/linked-list.py
+++ Neetcode/linked-list.py
@@ -193,3 +193,20 @@ class LRUCache:
         node.next = self.tail
         self.tail.prev.next = node
         self.tail.prev = node
+
+# https://leetcode.com/problems/copy-list-with-random-pointer/
+class Solution:
+    def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
+        oldToCopy = {None: None}
+        cur = head
+        while cur:
+            copy = Node(cur.val)
+            oldToCopy[cur] = copy
+            cur = cur.next
+        cur = head
+        while cur:
+            copy = oldToCopy[cur]
+            copy.next = oldToCopy[cur.next]
+            copy.random = oldToCopy[cur.random]
+            cur = cur.next
+        return oldToCopy[head]
