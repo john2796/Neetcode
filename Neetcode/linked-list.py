03ffa43901852de479e0801a730ef1abfd6e2320
john2708 <jbmiranda22796@gmail.com>
2024-06-23 16:34:18 -0700

    lru-cache

--- Neetcode/linked-list.py
+++ Neetcode/linked-list.py
@@ -141,59 +141,47 @@ class Solution:
 
 
 # https://leetcode.com/problems/lru-cache/
-class LinkedNode:
-    def __init__(self, key = -1, val = -1):
-        self.key = key
-        self.val = val
-        self.prev = None
-        self.next = None
+class Node:
+    def __init__(self, key, val):
+        self.key, self.val = key, val
+        self.prev = self.next = None
 
 class LRUCache:
     def __init__(self, capacity: int):
-        self.capacity = capacity
-        self.cache = dict()
-        self.head = LinkedNode()
-        self.tail = LinkedNode()
-        self.head.next = self.tail
-        self.tail.prev = self.head
+        self.cap = capacity
+        self.cache = {} # map key to node
 
+        self.left, self.right = Node(0, 0), Node(0, 0)
+        self.left.next, self.right.prev = self.right, self.left
+    # remove node from list
+    def remove(self, node):
+        prev, nxt = node.prev, node.next
+        prev.next, nxt.prev = nxt, prev
+    # insert node at right
+    def insert(self, node):
+        prev, nxt = self.right.prev, self.right
+        prev.next = nxt.prev = node
+        node.next, node.prev = nxt, prev
+    
     def get(self, key: int) -> int:
-        if key not in self.cache:
-            return -1 
-        node = self._evict(key)
-        self._addToEnd(node)
-        return node.val
+        if key in self.cache:
+            self.remove(self.cache[key])
+            self.insert(self.cache[key])
+            return self.cache[key].val
+        return -1
 
     def put(self, key: int, value: int) -> None:
-        if key in self.cache: 
-            node = self._delete(key)
-
-        node = LinkedNode(key, value)
-        self.cache[key] = node
-        self._addToEnd(node)
-
-        if len(self.cache) > self.capacity:
-            self._delete(self.head.next.key)
-
-    def _evict(self, key) -> LinkedNode:
-        node = self.cache[key]
-        node.prev.next = node.next
-        node.next.prev = node.prev
-        node.next = None
-        node.prev = None
-        return node
-    
-    def _delete(self, key) -> None:
-        deleteNode = self._evict(key)
-        del deleteNode
-        del self.cache[key]
-    
-    def _addToEnd(self, node) -> None:
-        node.prev = self.tail.prev
-        node.next = self.tail
-        self.tail.prev.next = node
-        self.tail.prev = node
-
+        if key in self.cache:
+            self.remove(self.cache[key])
+        self.cache[key] = Node(key, value)
+        self.insert(self.cache[key])
+        
+        if len(self.cache) > self.cap:
+            # remove the list and delete the LRU from hashmap
+            lru = self.left.next
+            self.remove(lru)
+            del self.cache[lru.key]
+         
 # https://leetcode.com/problems/copy-list-with-random-pointer/
 class Solution:
     def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
