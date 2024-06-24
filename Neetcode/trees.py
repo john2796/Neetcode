356572370fcea56e2e00a7e8fb18b558b3e2cd5e
john2708 <jbmiranda22796@gmail.com>
2024-06-23 17:08:08 -0700

    invert-binary-tree

---
+++ Neetcode/trees.py
@@ -0,0 +1,10 @@
+# https://leetcode.com/problems/invert-binary-tree/
+class Solution:
+    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        if not root:
+            return None
+        # swp children
+        root.left, root.right = root.right, root.left
+        self.invertTree(root.left)
+        self.invertTree(root.right)
+        return root
