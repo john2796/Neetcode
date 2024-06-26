e91e6673170e89922816c9c83c7badd5b7b17325
john2708 <jbmiranda22796@gmail.com>
2024-06-25 20:20:15 -0700

    Diameter of Binary Tree

--- Neetcode/tree.cpp
+++ Neetcode/tree.cpp
@@ -1,14 +1,3 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
     TreeNode* invertTree(TreeNode* root) {
@@ -27,4 +16,34 @@ public:
 
         return root;
     }
+};
+
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        if(!root) return 0;
+        int max_left = maxDepth(root->left);
+        int max_right = maxDepth(root->right);
+        return max(max_left, max_right) + 1;
+    }
+};
+
+// 543. Diameter of Binary Tree
+class Solution {
+  public:
+    int diameterOfBinaryTree(TreeNode* root){
+    int result = 0;
+    dfs(root, result);
+    return result;
+  }
+
+  private:
+    int dfs(TreeNode* root, int& result) {
+    if (root == NULL) return 0;
+
+    int left = dfs(root->left, result);
+    int right = dfs(root->right, result);
+    result = max(result, left+right);
+    return 1 + max(left, right);
+  }
 };
