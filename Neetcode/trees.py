6b6ed5f3667c8e2ec97a67e87817fa217ecf67dc
john2796 <jbmiranda22796@gmail.com>
2024-06-26 13:44:53 -0700

    max depth + templates for recursive dfs, iterative dfs, bfs

--- Neetcode/trees.py
+++ Neetcode/trees.py
@@ -1,10 +1,62 @@
-# https://leetcode.com/problems/invert-binary-tree/
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+
 class Solution:
     def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
         if not root:
-            return None
-        # swp children
+            return root
+        # swap
         root.left, root.right = root.right, root.left
+
         self.invertTree(root.left)
         self.invertTree(root.right)
-        return rootj
+        return root
+
+
+# 3 ways to solve maxDepth
+# Recursive DFS (template)
+class Solution:
+    def maxDepth_recursive_DFS(self, root: TreeNode) -> int:
+        if not root:
+            return 0
+        return 1 + max(
+            self.maxDepth_recursive_DFS(root.left),
+            self.maxDepth_recursive_DFS(root.right),
+        )
+
+
+# Iterative DFS (template)
+class Solution:
+    def maxDepth_iterative_DFS(self, root: TreeNode) -> int:
+        stack = [[root, 1]]
+        res = 0
+
+        while stack:
+            node, depth = stack.pop()
+            if node:
+                res = max(res, depth)  # add logic here
+                stack.append([node.left, depth + 1])
+                stack.append([node.right, depth + 1])
+        return res
+
+
+# BFS (template)
+class Solution:
+    def maxDepth_BFS(self, root: TreeNode) -> int:
+        q = deque()
+        if root:
+            q.append(root)
+        level = 0
+        while q:
+            for i in range(len(q)):
+                node = q.popleft()
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+            level += 1  # add logic for level here
+        return level
