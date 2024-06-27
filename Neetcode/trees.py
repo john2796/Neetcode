2ac71d735d31bc3c1be3e7482bdbe25888a486d4
john2796 <jbmiranda22796@gmail.com>
2024-06-27 15:46:18 -0700

    kthSmallest

--- Neetcode/trees.py
+++ Neetcode/trees.py
@@ -242,3 +242,24 @@ class Solution:
                 return False
             return dfs(node.left, left , node.val) and dfs(node.right, node.val , right)
         return dfs(root, float("-inf"), float("inf"))
+    
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
+        stack = []
+        curr = root
+        while stack or root:
+            while curr:
+                stack.append(curr)
+                curr = curr.left
+            curr = stack.pop()
+            k -= 1
+            if k == 0:
+                return curr.val
+            curr = curr.right
+        
