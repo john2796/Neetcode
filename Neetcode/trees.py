6d061f0fd6effac09359acee577a6eb800a42942
john2708 <jbmiranda22796@gmail.com>
2024-06-27 16:22:36 -0700

    buildTree

--- Neetcode/trees.py
+++ Neetcode/trees.py
@@ -263,3 +263,23 @@ class Solution:
                 return curr.val
             curr = curr.right
         
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+# preorder -> will always be the root 
+# inorder -> go through three in order left to right
+class Solution:
+    def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
+        if not preorder or not inorder:
+            return None
+        
+        root = TreeNode(preorder[0])
+        mid = inorder.index(preorder[0])
+        root.left = self.buildTree(preorder[1 : mid + 1], inorder[:mid])
+        root.right = self.buildTree(preorder[mid + 1 :], inorder[mid+1 :])
+        return root
+            
