1c6052bb5dee4b102180fe234845470c5be6ebda
john2796 <jbmiranda22796@gmail.com>
2024-07-02 14:47:20 -0700

    intersect

--- daily_leetcode_challenge/july_2024.py
+++ daily_leetcode_challenge/july_2024.py
@@ -10,3 +10,25 @@ class Solution:
             if count == 3:
                 return True
         return False
+
+
+class Solution:
+    def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        nums1.sort()
+        nums2.sort()
+        res = []
+        i, j = 0, 0
+        while i < len(nums1) and j < len(nums2):
+            if nums1[i] < nums2[j]:
+                i += 1
+            elif nums1[i] > nums2[j]:
+                j += 1
+            else:
+                res.append(nums1[i])
+                i += 1
+                j += 1
+        return res
+
+
+# {4: 1, 9: 1, 5: 1}
+# {9: 2, 4: 2, 8: 1}
