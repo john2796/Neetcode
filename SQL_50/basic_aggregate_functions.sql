95e27ef4c2d29f477a598c5c44197628ef2d8614
john2708 <jbmiranda22796@gmail.com>
2024-07-03 17:10:22 -0700

    basic aggregate sql

--- SQL_50/basic_aggregate_functions.sql
+++ SQL_50/basic_aggregate_functions.sql
@@ -11,3 +11,23 @@ left join UnitsSold u
 on p.product_id=u.product_id
 and u.purchase_date between p.start_date and p.end_date
 group by product_id;
+
+-- Write your MySQL query statement below
+-- SQL query that reports the average experience years of all the employees for each project, rounded to 2 digits.
+
+-- we want to add experienced_years to Project that's why we select from Project and left join the Employee
+SELECT p.project_id, ROUND(AVG(e.experience_years), 2) as average_years
+FROM Project p 
+LEFT JOIN Employee e
+ON p.employee_id = e.employee_id
+GROUP BY p.project_id; -- group by will calculate the average automatically
+
+-- Write your MySQL query statement below
+-- find the percentage of the users registered in each contest rounded to two decimals.
+-- Return the result table ordered by percentage in descending order. In case of a tie, order it by contest_id in ascending order.
+select 
+contest_id,
+round(count(distinct user_id) * 100 / (select count(user_id) from Users), 2) as percentage
+from Register
+group by contest_id
+order by percentage desc, contest_id;
--- daily_leetcode_challenge/july_2024.py
+++ daily_leetcode_challenge/july_2024.py
@@ -32,3 +32,20 @@ class Solution:
 
 # {4: 1, 9: 1, 5: 1}
 # {9: 2, 4: 2, 8: 1}
+        
+class Solution:        
+    def minDifference(self, nums: List[int]) -> int:
+        n = len(nums)
+        if n <= 4:
+            return 0
+
+        nums.sort()
+        # evaluate the minimum difference possible with at most 3 moves
+        min_diff = min(
+            nums[n-1] - nums[3], # change 3 smallest element
+            nums[n-2] - nums[2], # change 2 smallest and 1 largest element
+            nums[n-3] - nums[1], # change 1 smallest and 2 largest elements
+            nums[n-4] - nums[0]  # change 3 largest element
+        )
+        return min_diff
+        
