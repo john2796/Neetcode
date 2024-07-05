6808b1cd7a4e9a739a2c9c2d5e8d179df7d9723b
john2708 <jbmiranda22796@gmail.com>
2024-07-04 22:14:39 -0700

    immediate-food-delivery-ii

--- SQL_50/basic_aggregate_functions.sql
+++ SQL_50/basic_aggregate_functions.sql
@@ -67,3 +67,13 @@ SELECT
 FROM Transactions
 -- Group the results by month and country
 GROUP BY month, country;
+
+-- https://leetcode.com/problems/immediate-food-delivery-ii/?envType=study-plan-v2&envId=top-sql-50
+Select
+    round(avg(order_date = customer_pref_delivery_date) * 100, 2) as immediate_percentage
+from Delivery
+where (customer_id, order_date) in (
+    Select customer_id, min(order_date)
+    from Delivery
+    group by customer_id
+)
