e1380696069acd4b8ebfbda84ae347c0e3479001
john2796 <jbmiranda22796@gmail.com>
2024-07-05 11:09:17 -0700

    find-the-minimum-and-maximum-number-of-nodes-between

--- daily_leetcode_challenge/july_2024.py
+++ daily_leetcode_challenge/july_2024.py
@@ -32,8 +32,9 @@ class Solution:
 
 # {4: 1, 9: 1, 5: 1}
 # {9: 2, 4: 2, 8: 1}
-        
-class Solution:        
+
+
+class Solution:
     def minDifference(self, nums: List[int]) -> int:
         n = len(nums)
         if n <= 4:
@@ -42,10 +43,53 @@ class Solution:
         nums.sort()
         # evaluate the minimum difference possible with at most 3 moves
         min_diff = min(
-            nums[n-1] - nums[3], # change 3 smallest element
-            nums[n-2] - nums[2], # change 2 smallest and 1 largest element
-            nums[n-3] - nums[1], # change 1 smallest and 2 largest elements
-            nums[n-4] - nums[0]  # change 3 largest element
+            nums[n - 1] - nums[3],  # change 3 smallest element
+            nums[n - 2] - nums[2],  # change 2 smallest and 1 largest element
+            nums[n - 3] - nums[1],  # change 1 smallest and 2 largest elements
+            nums[n - 4] - nums[0],  # change 3 largest element
         )
         return min_diff
-        
+
+
+class Solution:
+    def mergeNodes(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head.next:
+            return None
+        ptr = head.next
+        sum = 0
+        while ptr.val != 0:
+            sum += ptr.val
+            ptr = ptr.next
+        head.next.val = sum
+        head.next.next = self.mergeNodes(ptr)
+        return head.next
+
+
+# https://leetcode.com/problems/find-the-minimum-and-maximum-number-of-nodes-between-critical-points/description/?envType=daily-question&envId=2024-07-05
+class Solution:
+    def nodesBetweenCriticalPoints(self, head: Optional[ListNode]) -> List[int]:
+        pre = head
+        cur = head.next
+        ans = [-1, -1]
+        prePosition, curPosition, firstPosition, position = -1, -1, -1, 0
+
+        while cur.next is not None:
+            if (cur.val < pre.val and cur.val < cur.next.val) or (
+                cur.val > pre.val and cur.val > cur.next.val
+            ):
+                # local
+                prePosition = curPosition
+                curPosition = position
+
+                if firstPosition == -1:
+                    firstPosition = position
+                if prePosition != -1:
+                    if ans[0] == -1:
+                        ans[0] = curPosition - prePosition
+                    else:
+                        ans[0] = min(ans[0], curPosition - prePosition)
+                    ans[1] = position - firstPosition
+            position += 1
+            pre = cur
+            cur = cur.next
+        return ans
