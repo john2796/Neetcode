2871aa005a17532cc8062669ae0b9adf99fbcb8e
john2796 <jbmiranda22796@gmail.com>
2024-07-12 14:00:49 -0700

    maximumGain

--- daily_leetcode_challenge/july_2024.py
+++ daily_leetcode_challenge/july_2024.py
@@ -190,3 +190,39 @@ class Solution:
 
         # Join all the characters in the result list to form the final output string
         return "".join(res)
+
+
+# https://leetcode.com/problems/maximum-score-from-removing-substrings/?envType=daily-question&envId=2024-07-12
+class Solution:
+    def maximumGain(self, s: str, x: int, y: int) -> int:
+        # Greedy with stack
+        res = 0
+        if y > x:
+            top = "ba"
+            top_score = y
+            bot = "ab"
+            bot_score = x
+        else:
+            top = "ab"
+            top_score = x
+            bot = "ba"
+            bot_score = y
+        
+        # removing first top substring cause they give more points
+        stack = []
+        for char in s:
+            if char == top[1] and stack and stack[-1] == top[0]:
+                res += top_score
+                stack.pop() # delete the first char of this substring
+            else:
+                stack.append(char)
+        
+        # removing bot substring cause they give less or equal amount of scores
+        new_stack = []
+        for char in stack:
+            if char == bot[1] and new_stack and new_stack[-1] == bot[0]:
+                res += bot_score
+                new_stack.pop()
+            else:
+                new_stack.append(char)
+        return res
