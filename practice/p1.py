f9742cc9b4205c07b7278212205ec4edb6e2bd25
john2796 <jbmiranda22796@gmail.com>
2024-07-18 01:26:30 -0700

    reverseKGroup

--- practice/p1.py
+++ practice/p1.py
@@ -853,7 +853,32 @@ class Solution:
         return head.next
 
 # reverse nodes in k group
-            
+# Approach: Reverse nodes in groups of k using iterative or recursive approach.
+class Solution:
+    def reverseKGroup(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
+        dummy = ListNode(0, head)
+        groupPrev = dummy
+        while True:
+            kth = self.getKth(groupPrev, k)
+            if not kth:
+                break
+            groupNext = kth.next
+            # reverse group
+            prev, curr = kth.next, groupPrev.next
+            while curr != groupNext:
+                tmp = curr.next
+                curr.next = prev
+                prev = curr
+                curr = tmp
+            tmp = groupPrev.next
+            groupPrev.next = kth
+            groupPrev = tmp
+        return dummy.next
+    def getKth(self, curr, k):
+        while curr and k > 0:
+            curr = curr.next
+            k -= 1
+        return curr            
 
 # ----- Trees
 # invert binary tree
