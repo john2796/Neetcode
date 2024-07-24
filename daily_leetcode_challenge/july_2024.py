08aaf58ab4e3a457459701b9e8743bf7c9afd86e
john2796 <jbmiranda22796@gmail.com>
2024-07-23 19:29:35 -0700

    spelling

--- daily_leetcode_challenge/july_2024.py
+++ daily_leetcode_challenge/july_2024.py
@@ -643,7 +643,7 @@ Output: [338,38,991]
 input digit list  [ 0, 1 ,2, 3, 4, 5, 6, 7, 8, 9 ]
 mapped digit list [ 8, 9, 4, 0, 2, 1, 3, 5, 7, 6]
 
-convert to string and chanage each digit to:
+convert to string and change each digit to:
  991 -> 669
  339 -> 007
  38 ->  07
