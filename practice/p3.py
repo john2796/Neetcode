151e9225af3fb6efd7fb84d813a6ec947855da76
john2796 <jbmiranda22796@gmail.com>
2024-08-07 06:15:54 -0700

    trie

--- practice/p3.py
+++ practice/p3.py
@@ -920,8 +920,132 @@ class Solution:
     return res
 # ----- Tries
 # implement trie prefix
+class TrieNode:
+  def __init__(self):
+    self.children = [None] * 26
+    self.end = False
+class Trie:
+  def __init__(self):
+    self.root = TrieNode()
+
+  def insert(self, word: str) -> None:
+    curr = self.root
+    for c in word:
+      i = ord(c) - ord("a")
+      if curr.children[i] == None:
+        curr.children[i] = TrieNode()
+      curr = curr.children[i]
+    curr.end = True
+
+  def search(self, word:str) -> bool:
+    curr = self.root
+    for c in word:
+      i = ord(c) - ord("a")
+      if curr.children[i] == None:
+        return False
+      curr = curr.children[i]
+    return curr.end
+
+  def startsWith(self, prefix) -> bool:
+    curr = self.root
+    for c in prefix:
+      i = ord(c) - ord("a")
+      if curr.children[i] == None:
+        return False
+      curr = curr.children[i]
+    return True
+
 # design add and search words data
+class TrieNode2:
+  def __init__(self):
+    self.children = {}
+    self.end = False
+class WordDictionary:
+  def __init__(self):
+    self.root = TrieNode2()
+    
+  def addWord(self, word:str) -> None:
+    cur = self.root
+    for c in word:
+      if c not in cur.children:
+        cur.children[c] = TrieNode2()
+      cur = cur.children[c]
+    cur.word = True
+    
+  def search(self, word:str) -> bool:
+    def dfs(j, root):
+      cur = root
+      for i in range(j, len(word)):
+        c = word[i]
+        if c == ".":
+          for child in cur.children.values():
+            if dfs(i + 1, child):
+              return True
+          return False
+        else:
+          if c not in cur.children:
+            return False
+          cur = cur.children[c]
+      return cur.word
+    return dfs(0, self.root)
 # word search II
+class TrieNode3:
+  def __init__(self):
+    self.children = {}
+    self.isWord = False
+    self.refs = 0
+  
+  def addWord(self, word):
+    cur = self
+    cur.refs += 1
+    for c in word:
+      if c not in cur.children:
+        cur.children[c] = TrieNode3()
+      cur = cur.children[c]
+      cur.refs += 1
+    cur.isWord = True
+  
+  def removeWord(self, word):
+    cur = self
+    cur.refs -= 1
+    for c in word:
+      if c in cur.children:
+        cur = cur.children[c]
+        cur.refs -= 1
+class Solution:
+  def findWords(self, board:List[List[str]], words:List[str]) -> List[str]:
+    # dfs + trie
+    root = TrieNode3()
+    for w in words:
+      root.addWord(w)
+    rows, cols = len(board), len(board[0])
+    res, visit = set(), set()
+    def dfs(r, c, node, word):
+      if (
+        r not in range(rows) 
+        or c not in range(cols) 
+        or board[r][c] not in node.children
+        or node.children[board[r][c]].refs < 1
+        or (r, c) in visit
+      ):
+        return
+      visit.add((r, c))
+      node = node.children[board[r][c]]
+      word += board[r][c]
+      if node.isWord:
+        node.isWord = False
+        res.add(word)
+        root.removeWord(word)
+      dfs(r + 1, c, node, word)
+      dfs(r - 1, c, node, word)
+      dfs(r, c + 1, node, word)
+      dfs(r, c - 1, node, word)
+      visit.remove((r, c))
+
+    for r in range(rows):
+      for c in range(cols):
+        dfs(r, c, root, "")
+    return list(res)
 
 # ----- Graphs
 # number of islands
